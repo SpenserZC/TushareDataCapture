7f32c015cdacd294eb19bbc3c4c05f28ce1d6b8f
张程 <spenser@chinaventure.com.cn>
2021-12-03 18:06:20 +0800

    新增http工具

---
+++ src/main/java/com/zcforit/config/TuShareConfig.java
@@ -0,0 +1,14 @@
+package com.zcforit.config;
+
+/**
+ * @author zhang cheng
+ * @version 1.0
+ * @description: tushare 配置
+ * @date 2021/12/3 18:04
+ */
+
+public class TuShareConfig {
+
+
+
+}
---
+++ src/main/java/com/zcforit/utils/HttpUtils.java
@@ -0,0 +1,89 @@
+package com.zcforit.utils;
+
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+/**
+ * @author zhang cheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/12/3 17:31
+ */
+@Slf4j
+@Component
+public class HttpUtils {
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * get请求
+     *
+     * @param url       请求地址
+     * @param headerMap 头部信息
+     * @param resp      响应结果类型
+     * @return
+     */
+    public Object get(String url, Map<String, String> headerMap, Class<?> resp) {
+        log.info("send request: " + url);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        for (Map.Entry<String, String> stringStringEntry : headerMap.entrySet()) {
+            httpHeaders.add(stringStringEntry.getKey(), stringStringEntry.getValue());
+        }
+        HttpEntity httpEntity = new HttpEntity(httpHeaders);
+        ResponseEntity<?> result = restTemplate.exchange(url, HttpMethod.GET, httpEntity, resp);
+        return result.getBody();
+    }
+
+    /**
+     * post 请求
+     *
+     * @param url        请求地址
+     * @param headerMap  头信息
+     * @param jsonObject 请求参数 JSON
+     * @return JSONObject
+     */
+    public JSONObject post(String url, Map<String, String> headerMap, JSONObject jsonObject) {
+        log.info("send request: " + url);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        for (Map.Entry<String, String> stringStringEntry : headerMap.entrySet()) {
+            httpHeaders.add(stringStringEntry.getKey(), stringStringEntry.getValue());
+        }
+        HttpEntity httpEntity = new HttpEntity(jsonObject, httpHeaders);
+        JSONObject result = restTemplate.postForObject(url, httpEntity, JSONObject.class);
+        return result;
+    }
+
+
+    /**
+     * get 请求
+     * @param requestUrl
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public <T> T getEntity(String requestUrl,Class<T> clazz) {
+        try {
+            log.info("send request: " + requestUrl);
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(requestUrl,String.class);
+            if (responseEntity!=null && responseEntity.getStatusCodeValue()==200){
+                objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                return (T)objectMapper.readValue(responseEntity.getBody(), clazz);
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            log.error("获取第三方 API error {} ", e);
+            return null;
+        }
+    }
+}
